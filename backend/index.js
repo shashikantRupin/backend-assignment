98af7afadd921de6d6435ee3c68388cc950728d1
Shashikant Rupin <shashikantrupin123@gmail.com>
2024-02-04 12:32:08 +0530

    error-resolved

--- backend/index.js
+++ backend/index.js
@@ -16,9 +16,14 @@ app.use((err, req, res, next) => {
   }
   next();
 });
+//Basic route to check server is running or not
+app.get("/", (req, res) => {
+  res.json({ message: "server is running" });
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
